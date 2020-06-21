119b74e44e594405a27378ce7b135f5bdbe3753a
kotaroooo0 <k33asby@gmail.com>
2020-06-22 01:03:05 +0900

    Add https listener

--- service/aws_lb_listener.tf
+++ service/aws_lb_listener.tf
@@ -8,3 +8,14 @@ resource "aws_lb_listener" "http" {
     type             = "forward"
   }
 }
+
+resource "aws_lb_listener" "https" {
+    load_balancer_arn = aws_lb.lb.arn
+    port = "443"
+    protocol = "HTTPS"
+    default_action {
+        target_group_arn = aws_lb_target_group.http.arn
+        type = "forward"
+    }
+    certificate_arn = aws_acm_certificate.nasum.arn
+}
